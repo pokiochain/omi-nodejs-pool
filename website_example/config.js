53690d5fc831a075e5e56413163bb31759dcdd15
Pokio Project <pokiochain@gmail.com>
2025-08-01 12:59:36 +0200

    omi changes

---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var parentCoin = "Monero";
+
+var api = "https://xmr.pokio.xyz/api/";
+var poolHost = "xmr.pokio.xyz";
+
+var email = "support@poolhost.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+var facebook = "https://www.facebook.com/<YourPoolFacebook";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.gntl.cash/block/{id}";
+var transactionExplorer = "https://explorer.gntl.cash/tx/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
